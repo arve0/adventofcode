e9238945804a8c6a4e11f8a7438788f424e08ef7
arve0 <arve.seljebu@gmail.com>
2016-12-05 07:45:20 +0100

    day five, part two

--- 5.js
+++ 5.js
@@ -1,24 +1,60 @@
-var m = require('md5');
+var crypto = require('crypto');
+
+function m (str) {
+  var h = crypto.createHash('md5');
+  return h.update(str).digest('hex');
+}
+
 var assert = require('assert');
 
 var input = 'ffykfhsq';
 
-function findPassword (key, length) {
-  var i = 0;
-  var password = '';
+var t = Date.now();
 
-  while (password.length < length) {
-    var h = m(key + i);
-    if (h.slice(0, 5) === '00000') {
-      debugger;
-      password += h[5];
-    }
+function findPassword (key, length) {
+  var i = -1;
+  var password = [];
+  var found = 0;
+  process.stdout.write(arrToString(password, length));
+
+  while (found !== length) {
     i += 1;
+    var h = m(key + i);
+
+    if (h.indexOf('00000') === 0) {
+      debugger;
+      var j = parseInt(h[5], 10);
+
+      if (isNaN(j) || j >= length) {
+        continue;
+      }
+
+      // avoid overwriting
+      if (!password[j]) {
+        password[j] = h[6];
+        found += 1;
+        process.stdout.write('\r' + arrToString(password, length) + ' time: ' + (Date.now() - t));
+      }
+    }
   }
 
-  return password;
+  process.stdout.write('\n');
+
+  return arrToString(password, length);
 }
 
-assert.equal(findPassword('abc', 3), '18f');
+function arrToString (pass, length) {
+  var res = [];
+  for (var i = 0; i < length; i += 1) {
+    if (!pass[i]) {
+      res[i] = '_';
+    } else {
+      res[i] = pass[i];
+    }
+  }
+  return res.join('');
+}
+
+assert.equal(findPassword('abc', 8), '05ace8e3');
 
 console.log(findPassword(input, 8));
