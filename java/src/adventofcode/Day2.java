84b2a898b5e405ec503b698354093d2f7d45bf4c
arve0 <arve.seljebu@gmail.com>
2017-12-02 16:21:57 +0100

    java day 2 problem 2

--- java/src/adventofcode/Day2.java
+++ java/src/adventofcode/Day2.java
@@ -6,24 +6,59 @@ public class Day2 {
 		FileReader input = new FileReader("input2.txt");
 
 		KeyPad keypad = new KeyPad();
+		SickKeyPad skp = new SickKeyPad();
 
 		for (String line : input.readLines()) {
 			for (char ch : line.toCharArray()) {
+//				System.out.println(ch);
 				if (ch == 'U') {
 					keypad.up();
+					skp.up();
 				} else if (ch == 'R') {
 					keypad.right();
+					skp.right();
 				} else if (ch == 'D') {
 					keypad.down();
+					skp.down();
 				} else if (ch == 'L') {
 					keypad.left();
+					skp.left();
 				}
 			}
-			System.out.print(keypad.currentPos());
+//			System.out.print(keypad.currentPos());
+			System.out.print(skp.currentPos());
 		}
 		
 	}
 
+}
+
+class SickKeyPad extends KeyPad {
+	SickKeyPad() {
+		pos.y = 2;
+		pos.x = 0;
+	}
+	String[][] pad = {
+		{ null, null, "1", null, null },
+		{ null,  "2", "3",  "4", null },
+		{ "5",   "6", "7",  "8", "9"  },
+		{ null,  "A", "B",  "C", null },
+		{ null, null, "D", null, null },
+	};
+	String[][] getPad() {
+		return pad;
+	}
+	boolean inBounds(int newY, int newX) {
+//		System.out.println("new y: " + newY);
+//		System.out.println("new x: " + newX);
+		boolean ret = newY >= 0 && newY < pad.length &&
+				newX >= 0 && newX < pad[newY].length &&
+				pad[newY][newX] != null;
+//		if (ret) {
+//			System.out.println("new value: " + pad[newY][newX]);			
+//		}
+		return ret;
+	}
 }
 
 class KeyPad {
@@ -32,6 +67,9 @@ class KeyPad {
 			{"4", "5", "6"},
 			{"7", "8", "9"},
 	};
+	String[][] getPad() {
+		return pad;
+	}
 	Position pos = new Position();
 	void reset() {
 		pos.y = 1;
@@ -58,7 +96,7 @@ class KeyPad {
 		pos.x = inBounds(pos.y, newX) ? newX : pos.x; 
 	}
 	String currentPos() {
-		return pad[pos.y][pos.x];
+		return getPad()[pos.y][pos.x];
 	}
 }
 
