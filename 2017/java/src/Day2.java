889abdef742c9e8ad17135a656b744de3aa912c1
arve0 <arve.seljebu@gmail.com>
2017-12-05 15:43:59 +0100

    aoc 17 d2p2

--- 2017/java/src/Day2.java
+++ 2017/java/src/Day2.java
@@ -1,6 +1,6 @@
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 /**
  * Day2
@@ -8,11 +8,13 @@ import java.util.stream.Stream;
 public class Day2 {
 
   public static void main(String[] args) throws Exception {
-    Stream<String> rows = Utils.readLines("input2.txt");
+    List<String> rows = Utils.readLines("input2.txt").collect(Collectors.toList());
 
-    int chksum = rows.mapToInt(Day2::rowDiff).sum();
-
+    int chksum = rows.stream().mapToInt(Day2::rowDiff).sum();
     System.out.println(chksum);
+
+    int divsum = rows.stream().map(Day2::divisable).mapToInt(t -> t.first / t.second).sum();
+    System.out.println(divsum);
   }
 
   static int rowDiff(String row) {
@@ -21,5 +23,23 @@ public class Day2 {
     Integer min = Collections.min(nums);
 
     return max - min;
+  }
+
+  static Tuple<Integer, Integer> divisable(String row) {
+    List<Integer> nums = Utils.stringsToInt(row.split("\t"));
+
+    int len = nums.size();
+    for (int i = 0; i < len; i++) {
+      for (int j = 0; j < len; j++) {
+        if (i == j) { continue; }
+        int a = nums.get(i);
+        int b = nums.get(j);
+        if (a % b == 0) {
+          double c = (double) a / b;
+          return new Tuple<Integer,Integer>(a, b);
+        }
+      }
+    }
+    return new Tuple<Integer,Integer>(0, 0);
   }
 }
---
+++ 2017/java/src/Tuple.java
@@ -0,0 +1,8 @@
+public class Tuple<T, U> {
+  T first;
+  U second;
+  Tuple(T f, U s) {
+    first = f;
+    second = s;
+  }
+}
