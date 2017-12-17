23de9d8f23b2762b9785661de0f8c747ea0a7172
arve0 <arve.seljebu@gmail.com>
2017-12-17 01:02:32 +0100

    aoc 17 d14p2

--- 2017/java/src/Day14.java
+++ 2017/java/src/Day14.java
@@ -11,17 +11,65 @@ public class Day14 {
   public static void main(String[] args) {
     String input = "amgozmfv";
 
-    int count = 0;
+    List<List<Integer>> disk = new ArrayList<>();
     for (int i = 0; i < 128; i++) {
-      List<Integer> s = knotHash(input + "-" + i);
-      count += s.stream()
-        .map(Integer::toBinaryString)
-        .mapToInt(str -> {
-          return str.replaceAll("0", "").length();
-        })
-        .sum();
+      List<Integer> row = knotHash(input + "-" + i).stream()
+        .reduce(new ArrayList<Integer>(), (r, j) -> {
+          int jj = 7;
+          while (jj >= 0) {
+            int n = (j >> jj);
+            n %= 2;
+            r.add(n);
+            jj--;
+          }
+          return r;
+        }, (a, b) -> {
+          a.addAll(b);
+          return a;
+        });
+      disk.add(row);
     }
+
+    int count = 0;
+    int groups = 0;
+    for (int i = 0; i < disk.size(); i++) {
+      List<Integer> row = disk.get(i);
+      for (int j = 0; j < row.size(); j++) {
+        int ii = row.get(j);
+        count += ii != 0 ? 1 : 0;
+        if (ii == 1) {
+          markGroup(i, j, disk);
+          groups++;
+        }
+      }
+    }
+
     System.out.println(count);
+    System.out.println(groups);
+    // 1295 too high
+  }
+
+  static void markGroup(int i, int j, List<List<Integer>> disk) {
+    if (i < 0 || j < 0
+        || i >= disk.size()
+        || j >= disk.get(i).size()) {
+      return;
+    }
+
+    disk.get(i).set(j, 2);
+
+    if (i > 0 && disk.get(i - 1).get(j) == 1) {
+      markGroup(i - 1, j, disk);
+    }
+    if (j > 0 && disk.get(i).get(j - 1) == 1) {
+      markGroup(i, j - 1, disk);
+    }
+    if ((i + 1) < disk.size() && disk.get(i + 1).get(j) == 1) {
+      markGroup(i + 1, j, disk);
+    }
+    if ((j + 1) < disk.get(i).size() && disk.get(i).get(j + 1) == 1) {
+      markGroup(i, j + 1, disk);
+    }
   }
 
   static List<Integer> knotHash(String input) {
