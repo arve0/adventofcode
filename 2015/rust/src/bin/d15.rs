568999c847ffa815eb4e3fcc2a9626a1d4fafcd4
arve0 <arve.seljebu@gmail.com>
2018-08-11 17:09:08 +0200

    aoc 15 rust d15 p2

--- 2015/rust/src/bin/d15.rs
+++ 2015/rust/src/bin/d15.rs
@@ -5,8 +5,10 @@ fn main() {
   ];
 
   let score = solve(&mut test_ingredients);
-
   assert_eq!(score, 62842880);
+
+  let score = solve_500_calories(&mut test_ingredients);
+  assert_eq!(score, 57600000);
 
   let mut ingredients = vec![
     Ingredient::new(4, -2, 0, 0, 5),
@@ -14,7 +16,11 @@ fn main() {
     Ingredient::new(-1, 0, 5, 0, 6),
     Ingredient::new(0, 0, -2, 2, 1),
   ];
+
   let score = solve(&mut ingredients);
+  println!("{}", score);
+
+  let score = solve_500_calories(&mut ingredients);
   println!("{}", score);
 }
 
@@ -57,6 +63,30 @@ fn calc_score(ingredients: &Vec<Ingredient>, amounts: &Vec<isize>) -> usize {
   }
 
   product
+}
+
+fn solve_500_calories(ingredients: &mut Vec<Ingredient>) -> usize {
+  let mut max = 0;
+  for amounts in ingredient_combinations(ingredients.len(), 100) {
+    if calc_calories(ingredients, &amounts) != 500 {
+      continue;
+    }
+    let score = calc_score(ingredients, &amounts);
+
+    if score > max {
+      max = score;
+    }
+  }
+  max
+}
+
+fn calc_calories(ingredients: &Vec<Ingredient>, amounts: &Vec<isize>) -> usize {
+  let s: isize = ingredients.iter()
+    .zip(amounts)
+    .map(|(i, a)| i.calories * a)
+    .sum();
+
+  s as usize
 }
 
 struct Ingredient {
