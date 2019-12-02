f7842b9dad78ead5e2b630920c651ac550b089b6
Arve Seljebu <arve.seljebu@gmail.com>
2019-12-02 06:50:41 +0100

    2019 day 2

---
+++ 2019/rust/src/day02.rs
@@ -0,0 +1,62 @@
+use crate::lib;
+
+pub fn solve() -> (usize, usize) {
+    assert_eq!(state_machine(vec![1,9,10,3,2,3,11,0,99,30,40,50]), 3500);
+    assert_eq!(state_machine(vec![1,0,0,0,99]), 2);
+    assert_eq!(state_machine(vec![2,3,0,3,99]), 2);
+    assert_eq!(state_machine(vec![2,4,4,5,99,0]), 2);
+    assert_eq!(state_machine(vec![1,1,1,4,99,5,6,0,99]), 30);
+
+    let input: Vec<usize> = lib::read_input("input02.txt")
+        .iter()
+        .flat_map(|n| n.parse())
+        .collect();
+
+    let mut bin = input.clone();
+    bin[1] = 12;
+    bin[2] = 2;
+    let part1 = state_machine(bin);
+
+    // 19690720 <- wanted
+    for i in 0..100 {
+        for j in 0..100 {
+            let mut bin = input.clone();
+            bin[1] = i;
+            bin[2] = j;
+            let out = state_machine(bin);
+
+            if out == 19690720 {
+                println!("{} {} gave 19690720", i, j);
+                return (part1, 100 * i + j);
+            }
+        }
+    }
+
+    unreachable!();
+}
+
+fn state_machine(mut bin: Vec<usize>) -> usize {
+    let mut pos = 0;
+
+    while bin[pos] != 99 {
+        let op = bin[pos];
+        let a = bin[bin[pos + 1]];
+        let b = bin[bin[pos + 2]];
+        let out = bin[pos + 3];
+
+
+        if op == 1 {
+            bin[out] = a + b;
+        } else if op == 2 {
+            bin[out] = a * b;
+        } else {
+            dbg!(pos);
+            dbg!(op);
+            break;
+        }
+
+        pos += 4;
+    }
+
+    bin[0]
+}
--- 2019/rust/src/main.rs
+++ 2019/rust/src/main.rs
@@ -1,42 +1,11 @@
 mod lib;
+mod day01;
+mod day02;
 
 fn main() {
-    // day 1, part 1
-    assert_eq!(fuel_for_mass(12), 2);
-    assert_eq!(fuel_for_mass(14), 2);
-    assert_eq!(fuel_for_mass(1969), 654);
-    assert_eq!(fuel_for_mass(100756), 33583);
+    day01::solve();
 
-    let input: Vec<i64> = lib::read_input("input01.txt")
-        .iter()
-        .flat_map(|n| n.parse())
-        .collect();
-
-    let fuel = input.iter()
-        .fold(0, |total, &mass| total + fuel_for_mass(mass));
-
-    println!("Part 1, total amount of fuel: {}", fuel);
-
-    // day 1, part 2
-    assert_eq!(fuel_for_mass_and_fuel(12), 2);
-    assert_eq!(fuel_for_mass_and_fuel(1969), 966);
-    assert_eq!(fuel_for_mass_and_fuel(100756), 50346);
-
-    let fuel = input.iter()
-        .fold(0, |total, &mass| total + fuel_for_mass_and_fuel(mass));
-
-    println!("Part 2, total amount of fuel: {}", fuel);
-}
-
-fn fuel_for_mass(mass: i64) -> i64 {
-    mass / 3 - 2
-}
-
-fn fuel_for_mass_and_fuel(mut mass: i64) -> i64 {
-    let mut total = 0;
-    while mass > 6 {
-        mass = fuel_for_mass(mass);
-        total += mass
-    }
-    total
+    let (part1, part2) = day02::solve();
+    println!("day 2 part 1: {}", part1);
+    println!("day 2 part 2: {}", part2);
 }
