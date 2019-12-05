e55ddf2565d2436b945c66c2aa39e8c16b4c6b10
Arve Seljebu <arve.seljebu@gmail.com>
2019-12-05 22:25:51 +0100

    2019 day 5 part 2

--- 2019/rust/src/day05.rs
+++ 2019/rust/src/day05.rs
@@ -2,8 +2,8 @@ use crate::lib;
 
 pub fn solve() {
     assert_eq!(state_machine(vec![3,0,4,0,99], 1337), 1337);
-    assert_eq!(state_machine(vec![1002,4,3,4,33], 999), -2);
-    assert_eq!(state_machine(vec![1101,100,-1,4,0], 999), -2);
+    assert_eq!(state_machine(vec![1002,4,3,4,33], 999), 0);
+    assert_eq!(state_machine(vec![1101,100,-1,4,0], 999), 0);
 
     let input: Vec<i64> = lib::read_input("input05.txt", ",")
         .iter()
@@ -14,16 +14,55 @@ pub fn solve() {
     let bin = input.clone();
     let part1 = state_machine(bin, 1);
     println!("day 5 part 1: {}", part1);
+
+    // part 2
+    assert_eq!(state_machine(vec![3,9,8,9,10,9,4,9,99,-1,8], 8), 1);
+    assert_eq!(state_machine(vec![3,9,8,9,10,9,4,9,99,-1,8], 9), 0);
+
+    assert_eq!(state_machine(vec![3,9,7,9,10,9,4,9,99,-1,8], 7), 1);
+    assert_eq!(state_machine(vec![3,9,7,9,10,9,4,9,99,-1,8], 8), 0);
+
+    assert_eq!(state_machine(vec![3,3,1108,-1,8,3,4,3,99], 8), 1);
+    assert_eq!(state_machine(vec![3,3,1108,-1,8,3,4,3,99], 9), 0);
+
+    assert_eq!(state_machine(vec![3,3,1107,-1,8,3,4,3,99], 7), 1);
+    assert_eq!(state_machine(vec![3,3,1107,-1,8,3,4,3,99], 8), 0);
+
+    assert_eq!(state_machine(vec![3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], 0), 0);
+    assert_eq!(state_machine(vec![3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], 8), 1);
+
+    assert_eq!(state_machine(vec![3,3,1105,-1,9,1101,0,0,12,4,12,99,1], 0), 0);
+    assert_eq!(state_machine(vec![3,3,1105,-1,9,1101,0,0,12,4,12,99,1], 8), 1);
+
+    let less_equal_or_more_then_eight = vec![
+        3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+        1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+        999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99
+    ];
+    assert_eq!(state_machine(less_equal_or_more_then_eight.clone(), 5), 999);
+    assert_eq!(state_machine(less_equal_or_more_then_eight.clone(), 8), 1000);
+    assert_eq!(state_machine(less_equal_or_more_then_eight.clone(), 12), 1001);
+
+    let bin = input.clone();
+    let part2 = state_machine(bin, 5);
+    println!("day 5 part 2: {}", part2);
 }
 
 fn state_machine(mut bin: Vec<i64>, input: i64) -> i64 {
     let mut pos = 0;
     let mut i = 0;
+    let mut output = 0;
+
     loop {
         i += 1;
         if i > 1000_000 {
             println!("too many loops");
+            lib::print_vec(&bin);
             return -1;
+        } else if pos >= bin.len() {
+            println!("index too high");
+            lib::print_vec(&bin);
+            return -2;
         }
 
         let (op, ma, mb, _mout) = get_op_and_modes(pos, &bin);
@@ -47,20 +86,47 @@ fn state_machine(mut bin: Vec<i64>, input: i64) -> i64 {
             bin[a] = input;
             pos += 2;
         } else if op == 4 {
-            let a = get_param(pos + 1, &ParamMode::MEMORY, &bin);
-            let next_op = bin[pos + 2];
-            if next_op == 99 && a != 0 {
-                return a;
-            }
+            output = get_param(pos + 1, &ma, &bin);
             pos += 2;
+        } else if op == 5 {
+            let a = get_param(pos + 1, &ma, &bin);
+            let b = get_param(pos + 2, &mb, &bin) as usize;
+            if a != 0 {
+                pos = b;
+            } else {
+                pos += 3
+            }
+        } else if op == 6 {
+            let a = get_param(pos + 1, &ma, &bin);
+            let b = get_param(pos + 2, &mb, &bin) as usize;
+            if a == 0 {
+                pos = b;
+            } else {
+                pos += 3
+            }
+        } else if op == 7 {
+            let a = get_param(pos + 1, &ma, &bin);
+            let b = get_param(pos + 2, &mb, &bin);
+            let out = bin[pos + 3] as usize;
+            bin[out] = if a < b { 1 } else { 0 };
+            pos += 4;
+        } else if op == 8 {
+            let a = get_param(pos + 1, &ma, &bin);
+            let b = get_param(pos + 2, &mb, &bin);
+            let out = bin[pos + 3] as usize;
+            bin[out] = if a == b { 1 } else { 0 };
+            pos += 4;
         } else if op == 99 {
-            return -2;
+            break;
         } else {
             dbg!(pos);
             dbg!(op);
+            lib::print_vec(&bin);
             unreachable!();
         }
     }
+
+    output
 }
 
 #[derive(Debug)]
@@ -94,6 +160,12 @@ fn param_mode(n: i64) -> ParamMode {
 
 fn get_param(pos: usize, mode: &ParamMode, bin: &Vec<i64>) -> i64 {
     use ParamMode::*;
+    if pos >= bin.len() {
+        println!("index too high");
+        dbg!(pos);
+        lib::print_vec(&bin);
+        return -2;
+    }
     match mode {
         MEMORY => get_param(bin[pos] as usize, &IMMIDIATE, bin),
         IMMIDIATE => bin[pos],
--- 2019/rust/src/lib.rs
+++ 2019/rust/src/lib.rs
@@ -4,6 +4,7 @@ pub fn read_input(filename: &str, separator: &str) -> Vec<String> {
     fs::read_to_string(filename)
         .expect("unable to open file")
         .split(separator)
+        .map(|s| s.trim())
         .map(String::from)
         .collect()
 }
@@ -18,4 +19,11 @@ pub fn digits(mut n: i64) -> Vec<i64> {
     }
 
     digits
+}
+
+pub fn print_vec<T>(vec: &Vec<T>) where T: std::fmt::Display {
+    vec.iter().enumerate()
+        .for_each(|(i, n)| print!("{}={}, ", i, n));
+
+    println!("");
 }
